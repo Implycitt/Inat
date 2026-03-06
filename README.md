0d75fa6297b8d209c016c0120498b6935d2abdf3
Implycitt <qgbordelon@gmail.com>
2026-03-05 22:55:37 -0600

    feat: messenger functions; autologin automessaging

---
+++ README.md
@@ -0,0 +1,14 @@
+# Inat group project
+
+--- 
+
+environment variables:
+
+```
+EMAIL=yourInaturalistEmailHere
+PASSWORD=yourInaturalistPasswordHere
+```
+
+if running this script (for whatever reason), make sure that you have your email and password in the .env inside of the in/ folder.
+For an example see the example.env.
+
--- src/messenger.py
+++ src/messenger.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import os
+from dotenv import load_dotenv
 
-def connectUrl(url: str) -> bool:
-    driver = webdriver.Chrome()
+def connectUrl(driver: webdriver, url: str) -> bool:
     try:
         driver.get(url)
         return driver
@@ -15,15 +16,50 @@ def terminate(driver: webdriver) -> None:
 def typeIntoElement(element, text):
     return element.send_keys(text)
 
-def submit(driver):
-    submitButton = driver.find_element(By.TAG_NAME, "commit")
-    print(submitButton)
+def pressButton(buttonElement):
+    buttonElement.click()
 
 def getHeaderBody():
-    with open("message.txt", 'r') as file:
+    with open("./in/message.txt", 'r') as file:
         wholeFile = file.read()
-        header, body = wholeFile.split('\n\n')
-    return (header, body)
+        header, body = map(str, wholeFile.split('\n\n'))
+    return (header, body.strip('\n'))
 
-if __name__ == "__main__":
-    print(getHeaderBody())
+def login(driver: webdriver) -> None:
+    email, password = map(str, (os.getenv('EMAIL'), os.getenv('PASSWORD')))
+    connectUrl(driver, "https://www.inaturalist.org/login")
+
+    emailField = driver.find_element(By.ID, "user_email")
+    passwordField = driver.find_element(By.ID, "user_password")
+
+    typeIntoElement(emailField, email)
+    typeIntoElement(passwordField, password)
+
+    pressButton(driver.find_element(By.NAME, "commit"))
+
+def sendMessage(driver: webdriver) -> None:
+    header, body = map(str, getHeaderBody())
+
+    headerField = driver.find_element(By.ID, "message_subject")
+    bodyField = driver.find_element(By.ID, "message_body")
+
+    typeIntoElement(headerField, header)
+    typeIntoElement(bodyField, body)
+
+    pressButton(driver.find_element(By.NAME, "commit"))
+
+def Messenger() -> None:
+    load_dotenv(dotenv_path="./in/.env")
+    baseUrl = "https://www.inaturalist.org/messages/new?to="
+
+    with open("./out/users.txt", 'r') as file:
+        users: tuple() = tuple(line.strip('\n'), for line in file)
+
+    driver = webdriver.Chrome()
+    login(driver)
+
+    for user in users:
+        messageUrl = baseUrl + user
+        connectUrl(driver, messageUrl)
+        sendMessage(driver)
+
